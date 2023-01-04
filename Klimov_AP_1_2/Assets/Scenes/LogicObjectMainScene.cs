3b0e362bba747e26b6a98514bbbb9feff1c456a0
HackLag <docterback@gmail.com>
2023-01-04 17:16:20 +0300

    Complite homework_1.2

---
+++ Klimov_AP_1_2/Assets/Scenes/LogicObjectMainScene.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class LogicObjectMainScene : MonoBehaviour
+{
+    public bool isPosition;
+    public bool isRotation;
+    public bool isScale;
+
+    float xRotation;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPosition)
+        {
+            transform.position = transform.position + new Vector3(0.0f, 0.0f, 2.0f) * Time.deltaTime;
+        }
+        if (isRotation)
+        {
+            xRotation += Time.deltaTime * -5;
+            transform.rotation = Quaternion.Euler(xRotation, 0, 0);
+        }
+        if (isScale)
+        {
+            transform.localScale = transform.localScale + new Vector3(1.5f, 1.5f, 1.5f) * Time.deltaTime;
+        }
+    }
+}
